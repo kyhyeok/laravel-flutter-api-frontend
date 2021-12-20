9f400ee8965acf5e5c864a6f9190d2cb24a0173f
YoonHyeok KIm <kyhyeok>
2021-12-20 12:51:00 +0900

    Navigation: Links between Login and Register Screens

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_api_frontend/screens/login.dart';
+import 'package:flutter_api_frontend/screens/register.dart';
 
 void main() => runApp(MyApp());
 
@@ -7,47 +9,11 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Welcome to Flutter',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Login'),
-        ),
-        body: Container(
-          color: Theme.of(context).primaryColorDark,
-          child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Card(
-                  elevation: 8.0,
-                  margin: EdgeInsets.only(left: 16.0, right: 16.0),
-                  child: Padding(
-                    padding: EdgeInsets.all(16.0),
-                    child: Column(
-                      children: <Widget>[
-                        TextField(
-                          keyboardType: TextInputType.emailAddress,
-                          decoration: InputDecoration(
-                            labelText: 'Email',
-                          ),
-                        ),
-                        TextField(
-                          keyboardType: TextInputType.visiblePassword,
-                          decoration: InputDecoration(
-                            labelText: 'Password',
-                          ),
-                        ),
-                        ElevatedButton(
-                          onPressed: () => print('Login clocked'),
-                          child: Text('Login'),
-                          style: ElevatedButton.styleFrom(
-                              minimumSize: Size(double.infinity, 36)),
-                        ),
-                      ],
-                    ),
-                  ),
-                )
-              ]),
-        ),
-      ),
+      home: Login(),
+      routes: {
+        '/login': (context) => Login(),
+        '/register': (context) => Register(),
+      }
     );
   }
 }
