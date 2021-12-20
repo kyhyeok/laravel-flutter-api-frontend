e83a52634f5ad5d77f2c8698adaaf514dcac02ee
YoonHyeok KIm <kyhyeok>
2021-12-20 12:38:52 +0900

    Flutter App Visual Widgets

---
+++ lib/main.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Welcome to Flutter',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Login'),
+        ),
+        body: Container(
+          color: Theme.of(context).primaryColorDark,
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Card(
+                  elevation: 8.0,
+                  margin: EdgeInsets.only(left: 16.0, right: 16.0),
+                  child: Padding(
+                    padding: EdgeInsets.all(16.0),
+                    child: Column(
+                      children: <Widget>[
+                        TextField(
+                          keyboardType: TextInputType.emailAddress,
+                          decoration: InputDecoration(
+                            labelText: 'Email',
+                          ),
+                        ),
+                        TextField(
+                          keyboardType: TextInputType.visiblePassword,
+                          decoration: InputDecoration(
+                            labelText: 'Password',
+                          ),
+                        ),
+                        ElevatedButton(
+                          onPressed: () => print('Login clocked'),
+                          child: Text('Login'),
+                          style: ElevatedButton.styleFrom(
+                              minimumSize: Size(double.infinity, 36)),
+                        ),
+                      ],
+                    ),
+                  ),
+                )
+              ]),
+        ),
+      ),
+    );
+  }
+}
