8441bf7e3be5455a4aa1c11fd62b1b8bab665204
YoonHyeok KIm <kyhyeok>
2021-12-21 15:39:54 +0900

    Device Info and Login

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,6 +1,30 @@
+import 'dart:io';
+import 'package:flutter/services.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_api_frontend/providers/AuthProvider.dart';
+import 'package:provider/provider.dart';
+import 'package:device_info/device_info.dart';
 
-class Login extends StatelessWidget {
+class Login extends StatefulWidget {
+  Login();
+
+  @override
+  _LoginState createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+  late String deviceName;
+  String errorMessage = '';
+
+  @override
+  void initState() {
+    super.initState();
+    getDeviceName();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,41 +41,106 @@ class Login extends StatelessWidget {
                 margin: EdgeInsets.only(left: 16.0, right: 16.0),
                 child: Padding(
                   padding: EdgeInsets.all(16.0),
-                  child: Column(
-                    children: <Widget>[
-                      TextField(
-                        keyboardType: TextInputType.emailAddress,
-                        decoration: InputDecoration(
-                          labelText: 'Email',
-                        ),
-                      ),
-                      TextField(
-                        keyboardType: TextInputType.visiblePassword,
-                        decoration: InputDecoration(
-                          labelText: 'Password',
-                        ),
-                      ),
-                      ElevatedButton(
-                        onPressed: () => print('Login clocked'),
-                        child: Text('Login'),
-                        style: ElevatedButton.styleFrom(
-                            minimumSize: Size(double.infinity, 36)),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.only(top: 20.0),
-                        child: InkWell(
-                          onTap: () {
-                            Navigator.pushNamed(context, '/register');
+                  child: Form(
+                    key: _formKey,
+                    child: Column(
+                      children: <Widget>[
+                        TextFormField(
+                          keyboardType: TextInputType.emailAddress,
+                          controller: emailController,
+                          decoration: InputDecoration(
+                            labelText: 'Email',
+                          ),
+                          validator: (String? value) {
+                            if (value!.trim().isEmpty) {
+                              return 'Please enter email';
+                            }
+                            return null;
                           },
-                          child: Text('Register new user'),
                         ),
-                      )
-                    ],
+                        TextFormField(
+                          keyboardType: TextInputType.visiblePassword,
+                          controller: passwordController,
+                          obscureText: true,
+                          autocorrect: false,
+                          enableSuggestions: false,
+                          decoration: InputDecoration(
+                            labelText: 'Password',
+                          ),
+                          validator: (String? value) {
+                            if (value!.isEmpty) {
+                              return 'Please enter password';
+                            }
+
+                            return null;
+                          }
+                        ),
+                        ElevatedButton(
+                          onPressed: () => submit(),
+                          child: Text('Login'),
+                          style: ElevatedButton.styleFrom(
+                              minimumSize: Size(double.infinity, 36)),
+                        ),
+                        Text(errorMessage, style: TextStyle(color: Colors.red)),
+                        Padding(
+                          padding: EdgeInsets.only(top: 20.0),
+                          child: InkWell(
+                            onTap: () {
+                              Navigator.pushNamed(context, '/register');
+                            },
+                            child: Text('Register new user'),
+                          ),
+                        )
+                      ],
+                    ),
                   ),
                 ),
               )
             ]),
       ),
     );
+  }
+
+  Future<void> submit() async {
+    final form = _formKey.currentState;
+
+    if (!form!.validate()) {
+      return;
+    }
+
+    final AuthProvider provider = Provider.of<AuthProvider>(context, listen: false);
+    try {
+      String token = await provider.login(
+        emailController.text,
+        passwordController.text,
+        deviceName,
+      );
+    } catch (Exception) {
+      setState(() {
+        errorMessage = Exception.toString().replaceAll('Exception: ', '');
+      });
+    }
+  }
+
+  Future<void> getDeviceName() async {
+    final DeviceInfoPlugin deviceInfoPlugin = DeviceInfoPlugin();
+
+    try {
+      if (Platform.isAndroid) {
+        var build = await deviceInfoPlugin.androidInfo;
+        setState(() {
+          deviceName = build.model;
+        });
+      } else if (Platform.isIOS) {
+        var build = await deviceInfoPlugin.iosInfo;
+        setState(() {
+          deviceName = build.model;
+        });
+      }
+    } on PlatformException {
+      setState(() {
+        deviceName = 'Failed to get platform version';
+      });
+    }
   }
 }
